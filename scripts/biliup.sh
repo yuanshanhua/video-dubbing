12cb8041630e59344ec58039883a017a8ab3328c
yuanshanhua <>
2025-04-02 13:25:17 +0800

    chore: 添加 biliup 调用示例

---
+++ scripts/biliup.sh
@@ -0,0 +1,3 @@
+# 调用 biliup 投稿到 b 站.
+# 231 - 计算机技术分区 no-reprint 不允许转载
+biliup upload output/video.mkv --tid 231 --title '[AI中文配音] CMU15-799数据库专题: 查询优化 2025 (更新中)' --tag 课程,CS,CMU,数据库 --no-reprint 1
